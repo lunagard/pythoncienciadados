3c69c44a55901d7abcb916a1450086692cb5a904
Luna Gard <lunasueca@gmail.com>
2020-10-24 13:38:50 -0300

    Refiz o codigo, o codigo ta funcionando, agora falta colocar a interação com o úsuario

--- print_tabuleirovelha.py
+++ print_tabuleirovelha.py
@@ -1,63 +1,37 @@
 X = "x"
 O = "O"
 VAZIO = " "
-msgVitoria = "Parabéns você venceu!"
-msgEmpate = "Empate"
+tabuleiro = [X, O, O,
+             O, O, X,
+             X, O, X]
+
+alinhamento = False
+vencedor = VAZIO
+
+# Alinhamento horizontal
+for i in range(0, 9, 3):
+    if tabuleiro[i] == tabuleiro[i+1] == tabuleiro[i+2]:
+        alinhamento = True
+        vencedor = tabuleiro[i]
 
 
-tabuleiro = [VAZIO, VAZIO, VAZIO,
-             VAZIO, VAZIO, VAZIO,
-             VAZIO, VAZIO, VAZIO]
+# Alinhamento vertical
+if not alinhamento:
+    for i in range(3):
+        if tabuleiro[i] == tabuleiro[i+3] == tabuleiro[i+6]:
+            alinhamento = True
+            vencedor = tabuleiro[i]
 
 
-def verificacao():
+# Alinhamento cruzado
+if not alinhamento:
+    for i in range(0, 3, 2):
+        if tabuleiro[0+i] == tabuleiro[4] == tabuleiro[8-i]:
+            alinhamento = True
+            vencedor = tabuleiro[i]
 
-    # Verificação Horizontal
-    if tabuleiro[0:2] == X or tabuleiro[0:2] == O:
-        print(msgVitoria)
-    if tabuleiro[3:5] == X or tabuleiro[3:5] == O:
-        print(msgVitoria)
-    if tabuleiro[6:8] == X or tabuleiro[6:8] == O:
-        print(msgVitoria)
-
-    # Verificação Vertical
-    if tabuleiro[0] == tabuleiro[3] == tabuleiro[6]:
-        print(msgVitoria)
-    if tabuleiro[1] == tabuleiro[4] == tabuleiro[7]:
-        print(msgVitoria)
-    if tabuleiro[2] == tabuleiro[5] == tabuleiro[8]:
-        print(msgVitoria)
-
-    # Verificação cruzada
-    if tabuleiro[2] == tabuleiro[4] == tabuleiro[6]:
-        print(msgVitoria)
-    if tabuleiro[0] == tabuleiro[4] == tabuleiro[8]:
-        print(msgVitoria)
-
-
-def verificaoEmpate():
-    if casa != VAZIO:
-        print(msgEmpate)
-
-
-for casa in tabuleiro:
-
-    escolharPosicao = int(
-        input('Digite a posição que quer "de 0 a 8": \n'))
-    escolharUsuario = input('\n Escolhar "X" ou "O":')
-    tabuleiro[escolharPosicao] = escolharUsuario
-    verificacao()
-    verificaoEmpate()
-
-    # O usuario digita a casa que ele quer....
-    #
-    # se cada posição da lista fou diferente da minha constante
-    # VERIFICAR TODAS AS POSIÇÕES ANTES DE DÁ A RESPOSTA
-
-    # POSSO FAZER A VERIFIÇÃO PARA VER SE A POSIÇÃO É VAZIA ANTES E PRINTAR TODAS A POSIÇÕES DISPONIVEIS CADA VEZ QUE O USUARIO digita
-    # E FAZER UMA VERIFICAÇÃO QUANDO O USUARIO DIGITA
-
-    # toda vez que um usario digita um valor tera que atribuir esse valor na posição do tabuleiro que o usuario escolher. FEITO
-    # o jogador só pode colocar na posição que estiver vazia
-    # depois verificar se as posições não estão todas completas.
-    # se não tiver nenhum vazio  e nenhuma sequencia de 3 do mesmo valor o jogo termina empatado.
+if alinhamento:
+    print("Vencedor:", vencedor)
+else:
+    if not VAZIO in tabuleiro:
+        print("Deu velha: Grande clássico entre vocês!")
